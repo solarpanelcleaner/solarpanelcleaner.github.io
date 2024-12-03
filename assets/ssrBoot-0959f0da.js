4badd0205420d768a520b79bf4f80e5310b29eb7
okerefe <okerefe@gmail.com>
2024-12-03 05:48:24 +0100

    updates

---
+++ assets/ssrBoot-0959f0da.js
@@ -0,0 +1,1 @@
+import{s,a_ as t,l as e,b9 as n}from"./index-36e220db.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:e(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
